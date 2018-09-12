d0e96606ae4265f73adcfe623584298417a29f88
kimnasik97 <kimnasik97@gmail.com>
2018-09-12 14:49:26 +0900

    add remark in add.py

---
+++ problem02.cpp
@@ -0,0 +1,19 @@
+// problem02.cpp : ì½˜ì†” ?‘ìš© ?„ë¡œê·¸ë¨???€??ì§„ì…?ì„ ?•ì˜?©ë‹ˆ??
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+const double square_meter = 3.3058;
+
+int main()
+{
+	{
+		int e;
+	}
+
+    return 0;
+}
+
